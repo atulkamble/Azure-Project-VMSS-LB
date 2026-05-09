d8a4136243487b23ee9c0f03465967858589a59c
Atul Kamble <atul_kamble@hotmail.com>
2026-05-09 20:13:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-dpkg --configure -a
-apt-get -y update
-
-# install Apache2
-apt-get -y install apache2
-apt-get -y update
-apt-get -y install mysql-server
-apt-get -y install php libapache2-mod-php php-mysql
-# write some HTML
-cd /var/www/html/
-echo "<h1>hello from $(hostname -f)</h1>">/var/www/html/index.html
-
-# restart Apache
-apachectl restart
+apt update -y
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+cd /var/www/html || exit
+rm -f index.html
+hostname=$(hostname)
+echo "<h1>Webserver: $hostname</h1>" > index.html
